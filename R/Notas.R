e10ea3a521d575c1cb8e3cd1152062afe42e22cf
MiriamLL <miriamjlerma@gmail.com>
2021-05-04 14:49:37 +0200

    Actualizar materiales

---
+++ R/Notas.R
@@ -0,0 +1,21 @@
+#' Datos de libreta de bobo/piquero/kena Sula dactylatra en Rapa Nui
+#'
+#' Datos de libreta hechos durante muestreo de Sula dactylatra en Rapa Nui
+#' Incluye 10 observaciones de 3 variables
+#' Incluye datos de 10 individuos
+#'
+#' @docType data
+#'
+#' @usage data(Notas)
+#'
+#' @format Un data frame en formato rda, contiene datos de diez individuos. 
+#' Las columnas son IDs, Hora_inicio, Hora_final
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. (2020)
+#'
+#'
+#' @examples
+#' data(Notas)
+"Notas"
